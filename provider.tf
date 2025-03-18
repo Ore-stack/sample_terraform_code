1fbbe7ae9fda086bba0e1e84ede16d29f223a6ae
Lanche <oreoluwaosinowo@yahoo.com>
2025-03-18 15:19:37 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "5.22.0"
     }
   }
+  backend "s3" {
+    bucket        = "lanche-statefile-bucket"
+    region        = "us-east-1"
+    key           = "dev/state/terraform.tfstate"
+    table         = "lanche-statefile-table"
+  }
 }
 
 provider "aws" {
